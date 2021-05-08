cc6a08e16d0b6a3bd8c7a8005f4c10ee3e79982b
Alexander <al.x.falconer@gmail.com>
2021-05-08 15:34:32 -0400

    updates

--- SPA/index.js
+++ SPA/index.js
@@ -1,10 +1,14 @@
 const url = 'http://brianeno.needsyourhelp.org/draw'
 
 const cardText = document.getElementById('card-text'),
+        userText = document.getElementById('user-text'),
         userData = document.getElementById('user-data'),
         genCardBtn = document.getElementById('card-btn'),
+        clearBtn = document.getElementById('clear-user'),
         userBtn = document.getElementById('user-btn'),
         saveBtn = document.getElementById('save-btn');
+
+let dataStore = []
 
 function randomCard() {
     fetch(url)
@@ -14,55 +18,86 @@ function randomCard() {
     });
 }
 
-//RandomCard();
 genCardBtn.addEventListener('click', randomCard)
+clearBtn.addEventListener('click', clearUser)
+saveBtn.addEventListener('click', saveData, false);
 
-function newDilemma() {
-    let user  = document.getElementById("text-box").value;
-    if (user != null) {
-       randomCard()
-       clearText()
-    }
-}
 
 function clearText() {
-    document.getElementById('user-text').value = "";
-    document.getElementById('card-text').innerText = "";
+    userText.value = "";
+    cardText.innerText = "";
 }
 
+function clearUser() {
+    userData.innerHTML = "";
+    sessionStorage.clear();
+}
 
 function learnMore() {
     window.open('http://music.hyperreal.org/artists/brian_eno/osfaq2.html', '_blank')
 }
 
-//next to "draw card", save dilemma & strategy response
-//error if no input
-
 function checkInput() {
-    if (document.getElementById("user-text").value === "" )
+    if (userText.value === "" )
     {
-        document.getElementById("user-text").value = errorMessage()
+        userText.value = errorMessage()
     }
     else randomCard()
 }
 
 function errorMessage() {
     window.alert("A dilemma must be entered. Try again.")
-    return document.getElementById("user-text").value
+    return userText.value
 }
 
-function getInput() {
-    let userInput = document.getElementById('user-text').value
-    let cardStrategy = document.getElementById('card-text').innerText
-    return (`DILEMMA: ${userInput} \n
-    STRATEGY: ${cardStrategy}`)
-}  
+let user = new Array();
+    strategy = new Array();
 
-saveBtn.addEventListener('click', getInput, false);
-
-function displayData() {
-    Array.prototype.forEach.call
-    let data = []
-    userData.innerHTML = getInput()
-    return data
+function saveData() {
+    let userDilemma = userText.value;
+    let userStrategy = cardText.innerText;
+    user[user.length] = userDilemma;
+    strategy[strategy.length] = userStrategy;
+    displayData();
 }
+    
+function displayData(){
+    let data = ("<b>Dilemma : Strategy</b></br>");
+    for(let i = 0; i < user.length; i++) {
+        data +=user[i]+"&nbsp<b>:</b>&nbsp";
+    }
+    for(let i = 0; i < strategy.length; i++) {
+        data += strategy[i]+"<br>";
+    }
+    userData.innerHTML = data;
+}
+
+/*function saveData() {
+    newPair = {dilemma: userText.value, strategy: cardText.innerText};
+    dataStore = [...dataStore, newPair];
+    console.log(newPair);
+    displayData();
+}
+
+/*function displayData() {
+    userData.innerHTML = (console.log(dataStore));
+}*/
+
+/*function saveData() {
+    let dilemmaDilemma = userText.value;
+    let userStrategy = cardText.innerText;
+    dilemma[dilemma.length] = dilemmaDilemma;
+    strategy[strategy.length] = userStrategy;
+    displayData();
+}
+
+function displayData(){
+    let data = ("<b>Dilemma : Strategy</b></br>");
+    for(let i = 0; i < dilemma.length; i++) {
+        data +=dilemma[i]+"&nbsp<b>:</b>&nbsp";
+    }
+    for(let i = 0; i < strategy.length; i++) {
+        data += strategy[i]+"<br>";
+    }
+    userData.innerHTML = data
+}*/
