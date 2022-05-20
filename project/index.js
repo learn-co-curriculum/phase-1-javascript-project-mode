2f93ac042b48a7a3159ef6e4f91b78142d14b7cf
David Coomer <dmcoomer@hotmail.com>
2022-05-19 21:20:14 -0400

    initial commit

--- project/index.js
+++ project/index.js
@@ -1,1 +1,6 @@
-console.log("running script");
+const URL = "https://www.omdbapi.com/?t=Forrest+Gump&apikey=821e1418"
+const trailer = "https://www.youtube.com/watch?v=6hDkhw5Wkas"
+
+fetch(trailer)
+.then(resp => resp.json())
+.then( (data) => console.log(data.title) );
