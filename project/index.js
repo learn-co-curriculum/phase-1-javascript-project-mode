2f9739ebe949f657c632f7906f8638d66a9b2255
David Coomer <dmcoomer@hotmail.com>
2022-05-19 23:51:26 -0400

    movie project v0.0.1

--- project/index.js
+++ project/index.js
@@ -1,6 +1,26 @@
-const URL = "https://www.omdbapi.com/?t=Forrest+Gump&apikey=821e1418"
-const trailer = "https://www.youtube.com/watch?v=6hDkhw5Wkas"
+// const URL = 'https://www.omdbapi.com/?t=Forrest+Gump&apikey=821e1418'
+const form = document.querySelector('form');
+const h1 = document.querySelector('h1');
+const img = document.querySelector('img');
+const input = document.querySelector('input');
 
-fetch(trailer)
-.then(resp => resp.json())
-.then( (data) => console.log(data.title) );
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let title = input.value.split(" ").join('+');
+    let URL = `https://www.omdbapi.com/?t=${title}&apikey=821e1418`;
+
+    document.querySelector('.image-div').classList.remove('hidden');
+
+    getData(URL);
+}
+
+function getData(URL) {
+    fetch(URL)
+    .then(data => data.json())
+    .then(res => {
+        h1.textContent = res.Title
+        img.src = res.Poster;
+    })
+}
